f8d3c8646469dc1c0a1a51de6e27c6c09c2a6dad
Chris Hamilton <chris@tracktownsoftware.com>
2023-03-27 13:09:22 -0700

    Update post-create.sh

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,7 +1,2 @@
-## Enable local HTTPS for .NET
-## Trust HTTPS certificate on Linux using Edge or Chrome
-dotnet dev-certs https
-sudo -E dotnet dev-certs https -ep /usr/local/share/ca-certificates/aspnet/https.crt --format PEM
-
 ## Restore .NET packages and build the default solution
 dotnet restore
